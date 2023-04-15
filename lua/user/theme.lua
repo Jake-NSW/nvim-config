7d02ae22d42505fc3ae36201dd6f3a2f73c70a75
Jake K <55055481+Jake-NSW@users.noreply.github.com>
2023-04-16 09:08:24 +1000

    Change back to kanagawa theme, has more colour variation

--- lua/user/theme.lua
+++ lua/user/theme.lua
@@ -3,7 +3,7 @@ M = { }
 M.setup = function()
     vim.o.background = "dark"
 
-    local current = 'tokyonight'
+    local current = 'kanagawa'
     if current == 'kanagawa' then
         -- Kanagawa Options
         theme = require('kanagawa')
@@ -16,18 +16,27 @@ M.setup = function()
             statementStyle = { bold = true },
             typeStyle = {},
             transparent = false,         -- do not set background color
-            dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+            dimInactive = true,         -- dim inactive window `:h hl-NormalNC`
             terminalColors = true,       -- define vim.g.terminal_color_{0,17}
             colors = {                   -- add/modify theme and palette colors
                 palette = {},
                 theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
             },
             overrides = function(colors) -- add/modify highlights
-                return {}
+                local theme = colors.theme
+                return {
+                    TelescopeTitle = { fg = theme.ui.special, bold = true },
+                    TelescopePromptNormal = { bg = theme.ui.bg_p1 },
+                    TelescopePromptBorder = { fg = theme.ui.bg_p1, bg = theme.ui.bg_p1 },
+                    TelescopeResultsNormal = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m1 },
+                    TelescopeResultsBorder = { fg = theme.ui.bg_m1, bg = theme.ui.bg_m1 },
+                    TelescopePreviewNormal = { bg = theme.ui.bg_dim },
+                    TelescopePreviewBorder = { bg = theme.ui.bg_dim, fg = theme.ui.bg_dim },
+                }
             end,
-            theme = "wave",              -- Load "wave" theme when 'background' option is not set
-            background = {               -- map the value of 'background' option to a theme
-                dark = "wave",           -- try "dragon" !
+            theme = "wave",
+            background = {
+                dark = "wave",
                 light = "lotus"
             },
         })
